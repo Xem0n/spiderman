6b88a0abfde39406eeef0e4a7eaa5fcba935ac0b
Xem0n <biedukiewicz87@gmail.com>
2022-05-13 21:50:18 +0200

    init texture class

---
+++ Rendering/Texture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace spiderman.Rendering
+{
+    internal class Texture
+    {
+
+    }
+}
